9cd1ecba1e31e7bb8b51b9c8a2e6c10c3dc20573
George Flores <georgeflores886@gmail.com>
2021-03-30 11:40:51 -0400

    implemented mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,9 @@
 //CRUD operations
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID } = require('mongodb')
+
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -9,8 +12,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     if (err) return console.log('Unable to connect to database.')
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'John',
-        age: '22'
-    })
+    // db.collection('users').deleteMany({ age: 22 })
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err))
+
+    db.collection('tasks').deleteOne({ description: 'Start on Python project' })
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
 })
