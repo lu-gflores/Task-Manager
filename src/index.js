ff1208cd797d859566e91e640a2ac5c745dad87d
George Flores <georgeflores886@gmail.com>
2021-03-30 15:42:49 -0400

    created task model and post endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,19 @@ app.post('/users', (req, res) => {
         res.status(400).send(err)
     })
 })
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(200).send(task)
+    }).catch(err => {
+        res.status(400).send(err)
+    })
+
+})
+
+
 app.listen(port, () => {
     console.log('listening on Port: ' + port)
 })
