d34f6adc69d1710ca645f47eea762f887944c70f
George Flores <georgeflores886@gmail.com>
2021-03-31 10:26:04 -0400

    created get endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -13,23 +13,59 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.status(200).send(user)
+        res.status(201).send(user)
     }).catch(err => {
         res.status(400).send(err)
     })
 })
+
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.status(200).send(users)
+    }).catch(err => {
+        res.status(500).send()
+    })
+})
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then(user => {
+        if (!user) return res.status(404).send()
+
+        res.send(user)
+    }).catch(err => {
+        res.status(500).send()
+    })
+})
+
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.status(200).send(task)
+        res.status(201).send(task)
     }).catch(err => {
         res.status(400).send(err)
     })
 
 })
 
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(task => {
+        res.status(200).send(task)
+    }).catch(err => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then(task => {
+        if (!task) return res.status(404).send()
+        res.status(200).send(task)
+    }).catch(err => {
+        res.status(404).send()
+    })
+})
 
 app.listen(port, () => {
     console.log('listening on Port: ' + port)
