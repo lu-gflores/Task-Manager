8841fcf2e5aefd1a386514f8558bb086b4a5651f
George Flores <georgeflores886@gmail.com>
2021-04-02 15:56:17 -0400

    added filter, pagination, and sorting for get tasks route

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,33 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+//GET /tasks?completed
+//Get /tasks?limit=&skip=
+//GET /tasks?sortBy=createdAt_
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    //filters tasks that are completed or not
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.status(200).send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+
+        }).execPopulate()
+        res.status(200).send(req.user.tasks)
     } catch (err) {
         res.status(500).send(err)
     }
